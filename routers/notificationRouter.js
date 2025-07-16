3eb4fc212f7ae7dd3b217b0b84aa3fd1fa0274ad
thiziri <raguebthiziri@gmail.com>
2025-07-16 21:36:37 +0200

    Ajout du router des notifications

--- routers/notificationRouter.js
+++ routers/notificationRouter.js
@@ -4,7 +4,7 @@ const{protect}=require("../middleware/authMiddleware")
 const{adminAndProfsOnly}=require("../middleware/adminAndprofsOnly")
 const{adminOnly}=require("../middleware/adminOnly")
 const {sendNotification,getMyNotifications,deleteNotification}=require("../controllers/notificationController");
-router.post("/envoyerNotification",protect,sendNotification);//approuved
+router.post("/envoyerNotification",protect,sendNotification);//approuve
 router.get("/mesNotifs",protect,adminAndProfsOnly,getMyNotifications)//approuved
 router.delete("/supprimerNotif/:id",protect,adminOnly,deleteNotification)// approuved
 module.exports=router;
